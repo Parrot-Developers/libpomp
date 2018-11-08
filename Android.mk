f1f8d6a699f93638adbdef8bbabc62ee57e72926
Yves-Marie Morgan <yves-marie.morgan@parrot.com>
2018-11-08 15:02:36 +0100

    [DEV] Tag libpomp as vendor module for android
    Required to use VNDK

--- Android.mk
+++ Android.mk
@@ -3,6 +3,7 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE := libpomp
+LOCAL_VENDOR_MODULE := true
 LOCAL_MODULE_TAGS := optional
 LOCAL_PRELINK_MODULE := false
 LOCAL_EXPORT_C_INCLUDE_DIRS := $(LOCAL_PATH)/include
